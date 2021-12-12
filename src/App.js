d749c40d086c689013f8d4a771b935aaa76f2733
Satoshi Matsushima <>
2021-12-12 23:57:02 +0900

    merge files (preparering to learn Routing)

--- src/App.js
+++ src/App.js
@@ -1,16 +1,5 @@
-import Backdrop from './components/Backdrop';
-import Modal from './components/Modal';
-import Todo from './components/Todo';
-
 function App() {
-  return (
-    <div>
-      <h1>Hello, world</h1>
-      <Todo text="Learn React" />
-      <Todo text="Master React" />
-      <Todo text="Explore the full React course" />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
